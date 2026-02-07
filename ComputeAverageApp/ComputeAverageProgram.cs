ddebd1d05f64276cbf4b0dd858b125ea3c09e3fd
edoria.419982 <edoria.419982@ANNEX-PC07>
2026-02-07 10:01:08 +0800

    Add project files.

---
+++ ComputeAverageApp/ComputeAverageProgram.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ComputeAverageApp
+{
+    class ComputeAverageProgram
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int count = 5;
+            Console.WriteLine("Enter 5 grades separated by new line:");
+            for (int i = 0; i < count; i++)
+            {
+                if (int.TryParse(Console.ReadLine(), out int grade))
+                {
+                    sum += grade;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input, please enter number.");
+                    i--; 
+                }
+            }
+            double average = (double)sum / count;
+            int rAverage = (int)Math.Round(average); 
+
+            Console.WriteLine($"The average is {average} and round off to {rAverage}");
+            Console.WriteLine("Press any key to exit..........");
+            Console.ReadKey(true);
+        }
+    }
+}
+
