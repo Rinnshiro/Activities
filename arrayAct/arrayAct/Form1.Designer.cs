4883f3bf162b697f8d463070c5d5994cdd93fc06
Rinn <133751706+Rinnshiro@users.noreply.github.com>
2026-03-14 09:56:22 +0800

    Add files via upload

---
+++ arrayAct/arrayAct/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace arrayAct
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.classmateNameList = new System.Windows.Forms.ListBox();
+            this.showAll = new System.Windows.Forms.Button();
+            this.rowandcolumnsTexBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // classmateNameList
+            // 
+            this.classmateNameList.FormattingEnabled = true;
+            this.classmateNameList.ItemHeight = 19;
+            this.classmateNameList.Location = new System.Drawing.Point(45, 50);
+            this.classmateNameList.Name = "classmateNameList";
+            this.classmateNameList.Size = new System.Drawing.Size(162, 156);
+            this.classmateNameList.TabIndex = 0;
+            // 
+            // showAll
+            // 
+            this.showAll.Location = new System.Drawing.Point(125, 236);
+            this.showAll.Name = "showAll";
+            this.showAll.Size = new System.Drawing.Size(153, 54);
+            this.showAll.TabIndex = 1;
+            this.showAll.Text = "Click To Show";
+            this.showAll.UseVisualStyleBackColor = true;
+            this.showAll.Click += new System.EventHandler(this.showAll_Click);
+            // 
+            // rowandcolumnsTexBox
+            // 
+            this.rowandcolumnsTexBox.Location = new System.Drawing.Point(213, 50);
+            this.rowandcolumnsTexBox.Multiline = true;
+            this.rowandcolumnsTexBox.Name = "rowandcolumnsTexBox";
+            this.rowandcolumnsTexBox.Size = new System.Drawing.Size(162, 116);
+            this.rowandcolumnsTexBox.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 407);
+            this.Controls.Add(this.rowandcolumnsTexBox);
+            this.Controls.Add(this.showAll);
+            this.Controls.Add(this.classmateNameList);
+            this.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox classmateNameList;
+        private System.Windows.Forms.Button showAll;
+        private System.Windows.Forms.TextBox rowandcolumnsTexBox;
+    }
+}
+
---
+++ arrayAct/arrayAct/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace arrayAct
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void showAll_Click(object sender, EventArgs e)
+        {
+            string[] classmates = { "Cris", "Patrick", "Jhirro", "Rhobbie", "Justine", "Mark" };
+            classmateNameList.Items.Clear();
+            foreach (string name in classmates)
+            {
+                classmateNameList.Items.Add(name);
+            }
+
+            char[,] initial = new char[2, 3]
+            {
+        { 'C', 'P', 'J' },
+        { 'R', 'J', 'M' }
+            };
+
+
+            string gridOutput = "";
+            for (int row = 0; row < 2; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    gridOutput += initial[row, col] + " ";
+                }
+                gridOutput += Environment.NewLine;
+            }
+            rowandcolumnsTexBox.Text = gridOutput;
+
+
+            string message = "Hello everyone, welcome to class!";
+            bool containsHello = message.ToLower().Contains("hello");
+
+            MessageBox.Show($"{message}" +
+                $"\nDoes the message contain 'hello'? \n" +
+                $"{containsHello}");
+        }
+    }
+}
