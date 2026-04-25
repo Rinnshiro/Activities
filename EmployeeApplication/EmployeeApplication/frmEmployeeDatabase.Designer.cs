7346f3d878a068615295fa09f747a50453a43900
Rinn <133751706+Rinnshiro@users.noreply.github.com>
2026-04-25 08:32:34 +0800

    Add files via upload

---
+++ EmployeeApplication/EmployeeApplication/frmEmployeeDatabase.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace EmployeeApplication
+{
+    partial class frmEmployeeDatabase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.lblAddEmployee = new System.Windows.Forms.Label();
+            this.lblEmployeeID = new System.Windows.Forms.Label();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblPosition = new System.Windows.Forms.Label();
+            this.txtEmployeeID = new System.Windows.Forms.TextBox();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtPosition = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.lblEmployeeList = new System.Windows.Forms.Label();
+            this.listEmployees = new System.Windows.Forms.DataGridView();
+            this.columnId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnFirstName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnLastName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnPosition = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.listEmployees)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblAddEmployee
+            // 
+            this.lblAddEmployee.AutoSize = true;
+            this.lblAddEmployee.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAddEmployee.Location = new System.Drawing.Point(17, 17);
+            this.lblAddEmployee.Name = "lblAddEmployee";
+            this.lblAddEmployee.Size = new System.Drawing.Size(112, 20);
+            this.lblAddEmployee.TabIndex = 0;
+            this.lblAddEmployee.Text = "Add Employee:";
+            // 
+            // lblEmployeeID
+            // 
+            this.lblEmployeeID.AutoSize = true;
+            this.lblEmployeeID.Location = new System.Drawing.Point(18, 52);
+            this.lblEmployeeID.Name = "lblEmployeeID";
+            this.lblEmployeeID.Size = new System.Drawing.Size(74, 13);
+            this.lblEmployeeID.TabIndex = 1;
+            this.lblEmployeeID.Text = "Employee ID *";
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Location = new System.Drawing.Point(18, 95);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(62, 13);
+            this.lblFirstName.TabIndex = 2;
+            this.lblFirstName.Text = "First name *";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(18, 139);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(63, 13);
+            this.lblLastName.TabIndex = 3;
+            this.lblLastName.Text = "Last name *";
+            // 
+            // lblPosition
+            // 
+            this.lblPosition.AutoSize = true;
+            this.lblPosition.Location = new System.Drawing.Point(18, 182);
+            this.lblPosition.Name = "lblPosition";
+            this.lblPosition.Size = new System.Drawing.Size(51, 13);
+            this.lblPosition.TabIndex = 4;
+            this.lblPosition.Text = "Position *";
+            // 
+            // txtEmployeeID
+            // 
+            this.txtEmployeeID.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtEmployeeID.Location = new System.Drawing.Point(21, 68);
+            this.txtEmployeeID.Name = "txtEmployeeID";
+            this.txtEmployeeID.Size = new System.Drawing.Size(138, 20);
+            this.txtEmployeeID.TabIndex = 5;
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtFirstName.Location = new System.Drawing.Point(21, 111);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(138, 20);
+            this.txtFirstName.TabIndex = 6;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtLastName.Location = new System.Drawing.Point(21, 154);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(138, 20);
+            this.txtLastName.TabIndex = 7;
+            // 
+            // txtPosition
+            // 
+            this.txtPosition.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPosition.Location = new System.Drawing.Point(21, 198);
+            this.txtPosition.Name = "txtPosition";
+            this.txtPosition.Size = new System.Drawing.Size(138, 20);
+            this.txtPosition.TabIndex = 8;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnSubmit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSubmit.ForeColor = System.Drawing.Color.White;
+            this.btnSubmit.Location = new System.Drawing.Point(43, 234);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(94, 30);
+            this.btnSubmit.TabIndex = 9;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = false;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // lblEmployeeList
+            // 
+            this.lblEmployeeList.AutoSize = true;
+            this.lblEmployeeList.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmployeeList.Location = new System.Drawing.Point(189, 17);
+            this.lblEmployeeList.Name = "lblEmployeeList";
+            this.lblEmployeeList.Size = new System.Drawing.Size(94, 17);
+            this.lblEmployeeList.TabIndex = 10;
+            this.lblEmployeeList.Text = "Employee List:";
+            // 
+            // listEmployees
+            // 
+            this.listEmployees.AllowUserToAddRows = false;
+            this.listEmployees.AllowUserToDeleteRows = false;
+            this.listEmployees.BackgroundColor = System.Drawing.Color.Cornsilk;
+            this.listEmployees.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.listEmployees.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.columnId,
+            this.columnFirstName,
+            this.columnLastName,
+            this.columnPosition});
+            this.listEmployees.GridColor = System.Drawing.SystemColors.InactiveCaption;
+            this.listEmployees.Location = new System.Drawing.Point(192, 43);
+            this.listEmployees.Name = "listEmployees";
+            this.listEmployees.ReadOnly = true;
+            this.listEmployees.RowTemplate.Height = 25;
+            this.listEmployees.Size = new System.Drawing.Size(444, 242);
+            this.listEmployees.TabIndex = 11;
+            // 
+            // columnId
+            // 
+            this.columnId.DataPropertyName = "Id";
+            this.columnId.HeaderText = "id";
+            this.columnId.Name = "columnId";
+            this.columnId.ReadOnly = true;
+            // 
+            // columnFirstName
+            // 
+            this.columnFirstName.DataPropertyName = "FirstName";
+            this.columnFirstName.HeaderText = "firstName";
+            this.columnFirstName.Name = "columnFirstName";
+            this.columnFirstName.ReadOnly = true;
+            // 
+            // columnLastName
+            // 
+            this.columnLastName.DataPropertyName = "LastName";
+            this.columnLastName.HeaderText = "lastName";
+            this.columnLastName.Name = "columnLastName";
+            this.columnLastName.ReadOnly = true;
+            // 
+            // columnPosition
+            // 
+            this.columnPosition.DataPropertyName = "Position";
+            this.columnPosition.HeaderText = "position";
+            this.columnPosition.Name = "columnPosition";
+            this.columnPosition.ReadOnly = true;
+            // 
+            // frmEmployeeDatabase
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Tan;
+            this.ClientSize = new System.Drawing.Size(663, 336);
+            this.Controls.Add(this.listEmployees);
+            this.Controls.Add(this.lblEmployeeList);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtPosition);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.txtEmployeeID);
+            this.Controls.Add(this.lblPosition);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.lblEmployeeID);
+            this.Controls.Add(this.lblAddEmployee);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmEmployeeDatabase";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Employee Database";
+            ((System.ComponentModel.ISupportInitialize)(this.listEmployees)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAddEmployee;
+        private System.Windows.Forms.Label lblEmployeeID;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblPosition;
+        private System.Windows.Forms.TextBox txtEmployeeID;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtPosition;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label lblEmployeeList;
+        private System.Windows.Forms.DataGridView listEmployees;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnFirstName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnLastName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnPosition;
+    }
+}
---
+++ EmployeeApplication/EmployeeApplication/frmEmployeeDatabase.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using EmployeeNamespace;
+
+namespace EmployeeApplication
+{
+    public partial class frmEmployeeDatabase : Form
+    {
+        private BindingList<Employee> employeeList = new BindingList<Employee>();
+
+        public frmEmployeeDatabase()
+        {
+            InitializeComponent();
+            LoadEmployees();
+        }
+
+        private void LoadEmployees()
+        {
+            employeeList.Add(new Employee("20190001", "Raine", "Edoria", "Supervisor"));
+            employeeList.Add(new Employee("20160005", "Cris Erick", "Ibay", "Manager"));
+            employeeList.Add(new Employee("20210046", "Jhirro", "Meneses", "Staff"));
+
+            listEmployees.AutoGenerateColumns = false;
+            listEmployees.DataSource = employeeList;
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (txtEmployeeID.Text.Trim() == "" ||
+                txtFirstName.Text.Trim() == "" ||
+                txtLastName.Text.Trim() == "" ||
+                txtPosition.Text.Trim() == "")
+            {
+                MessageBox.Show("Please fill in all required fields.", "Missing Input",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            Employee emp = new Employee(
+                txtEmployeeID.Text.Trim(),
+                txtFirstName.Text.Trim(),
+                txtLastName.Text.Trim(),
+                txtPosition.Text.Trim()
+            );
+
+            employeeList.Add(emp);
+
+            txtEmployeeID.Clear();
+            txtFirstName.Clear();
+            txtLastName.Clear();
+            txtPosition.Clear();
+
+            txtEmployeeID.Focus();
+        }
+    }
+}
